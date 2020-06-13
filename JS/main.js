07a0b5fd61b509d6e4f6eaaf1f88fd76ed9e6d28
monchi316 <monchi316@gmail.com>
2020-06-13 22:23:23 +0900

    課題③-1修正しました

--- JS/main.js
+++ JS/main.js
@@ -4,30 +4,37 @@ const btn = document.getElementById('btn');
 const tasks = [];
 
 btn.addEventListener('click', () => {
-  var task = document.getElementById('task').value;
+  let task = document.getElementById('task').value;
   tasks.push(task);
   add();
 
 });
 
 function add() {
-  var tbody = document.querySelector('tbody');
-  var tr = document.createElement('tr');
+  let tbody = document.querySelector('tbody');
+  let tr = document.createElement('tr');
   tbody.appendChild(tr);
-  var td = document.createElement('td');
-  tr.appendChild(td);
-  td.textContent = tasks.length - 1;
-  var td = document.createElement('td');
-  tr.appendChild(td);
-  td.textContent = task.value;
-  var td = document.createElement('td');
-  tr.appendChild(td);
-  var workingbtn = document.createElement('button');
-  td.appendChild(workingbtn);
-  workingbtn.textContent = '作業中';
-  var td = document.createElement('td');
-  tr.appendChild(td);
-  var removebtn = document.createElement('button');
-  td.appendChild(removebtn);
-  removebtn.textContent = '削除';
+  tr.className = 'working';
+  
+  for(let i=0; i<4; i++) {
+    let td = document.createElement('td');
+    tr.appendChild(td);
+    if(i === 0) {
+      td.className = 'idNumber';
+      td.textContent = tasks.length - 1;
+    } else if (i === 1) {
+      td.textContent = task.value;
+    } else if (i === 2) {
+      let workingbtn = document.createElement('button');
+      td.appendChild(workingbtn);
+      workingbtn.textContent = '作業中';
+    } else if (i === 3) {
+      let removebtn = document.createElement('button');
+      td.appendChild(removebtn);
+      removebtn.textContent = '削除';
+    }
+  }
+
+  task.value = '';
+
 }
