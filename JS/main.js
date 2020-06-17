6393133e72a4157cc46522e5685d10ff1df5bcee
monchi316 <monchi316@gmail.com>
2020-06-18 01:58:04 +0900

    中括弧の修正

--- JS/main.js
+++ JS/main.js
@@ -6,46 +6,46 @@
   const tasks = [];
 
 // 追加ボタン
-btn.addEventListener('click', () => {  
-  addTask();  
-});
-
-
-
-// ↓関数内容↓
-
-// タスクの追加
-const addTask = () => {
-  const task = {
-    id: tasks.length,
-    taskName: document.getElementById('taskForm').value,
-    status: '作業中',
-  }
-  tasks.push(task);
-  
-  const tbody = document.querySelector('tbody');
-  const tr = document.createElement('tr');
-  tbody.appendChild(tr);
-  tr.className = 'working';
-  
-  Object.keys(task).forEach(key => {
-    const td = document.createElement('td');
-    tr.appendChild(td);
-    if (task[key] === '作業中') {
-      const workingbtn = document.createElement('button');
-      td.appendChild(workingbtn);
-      workingbtn.textContent = task.status;
-    } else {
-      td.textContent = task[key];
-    }
+  btn.addEventListener('click', () => {  
+    addTask();  
   });
 
-  const td = document.createElement('td');
-  tr.appendChild(td);
-  td.innerHTML = '<button name="removebtn">削除</button>';
 
-  taskForm.value = '';
-  
-}
+
+  // ↓関数内容↓
+
+  // タスクの追加
+  const addTask = () => {
+    const task = {
+      id: tasks.length,
+      taskName: document.getElementById('taskForm').value,
+      status: '作業中',
+    }
+    tasks.push(task);
+    
+    const tbody = document.querySelector('tbody');
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+    tr.className = 'working';
+    
+    Object.keys(task).forEach(key => {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+      if (task[key] === '作業中') {
+        const workingbtn = document.createElement('button');
+        td.appendChild(workingbtn);
+        workingbtn.textContent = task.status;
+      } else {
+        td.textContent = task[key];
+      }
+    });
+
+    const td = document.createElement('td');
+    tr.appendChild(td);
+    td.innerHTML = '<button name="removebtn">削除</button>';
+
+    taskForm.value = '';
+    
+  }
 
 }
