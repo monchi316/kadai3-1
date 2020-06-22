ebe553470584f890f412ef93f9b74c42719c3c6d
monchi316 <monchi316@gmail.com>
2020-06-22 23:33:33 +0900

    配列の繰り返し処理への変更分

--- JS/main.js
+++ JS/main.js
@@ -1,54 +1,50 @@
 'use strict'
-
 {
-
   const btn = document.getElementById('btn');
   const tasks = [];
-
-// 追加ボタン
-  btn.addEventListener('click', () => {  
-    addTask();  
+  const taskForm = document.getElementById('taskForm');
+  // 追加ボタン
+  btn.addEventListener('click', () => {
+    if (taskForm.value) {
+      addTask();
+    }
   });
-
-
-
   // ↓関数内容↓
-
   // タスクの追加
   const addTask = () => {
     const task = {
-      id: tasks.length,
-      taskName: document.getElementById('taskForm').value,
-      status: '作業中',
+      idNumber: '',
+      taskName: taskForm.value,
+      status: { 0: '作業中', 1: '完了' },
     }
     tasks.push(task);
-    
     const tbody = document.querySelector('tbody');
-    const tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    tr.className = 'working';
-    
-    Object.keys(task).forEach(key => {
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    tasks.forEach((task, index) => {
+      const tr = document.createElement('tr');
+      tbody.appendChild(tr);
+      tr.className = 'working';
+      const itd = document.createElement('td');
+      tr.appendChild(itd);
+      itd.textContent = index;
+      const ttd = document.createElement('td');
+      tr.appendChild(ttd);
+      ttd.textContent = task.taskName;
+      const std = document.createElement('td');
+      tr.appendChild(std);
+      const statusbtn = document.createElement('button');
+      std.appendChild(statusbtn);
+      statusbtn.name = 'statusbtn';
+      statusbtn.textContent = task.status[0];
       const td = document.createElement('td');
       tr.appendChild(td);
-      if (task[key] === '作業中') {
-        const workingbtn = document.createElement('button');
-        td.appendChild(workingbtn);
-        workingbtn.textContent = task.status;
-      } else {
-        td.textContent = task[key];
-      }
+      const removebtn = document.createElement('button');
+      td.appendChild(removebtn);
+      removebtn.name = 'removebtn';
+      removebtn.textContent = '削除';
     });
-
-    const td = document.createElement('td');
-    tr.appendChild(td);
-    const removebtn = document.createElement('button');
-    td.appendChild(removebtn);
-    removebtn.textContent = task.status;
-    removebtn.name = "removebtn";
-
     taskForm.value = '';
-    
   }
-
 }
